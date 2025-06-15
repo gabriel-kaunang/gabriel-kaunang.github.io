63d3296ba563bc4657fb5f2b338c496c226057b2
Gabriel Kaunang <gabrielkaunang@mail.ugm.ac.id>
2025-06-15 14:13:11 -0500

    updated publications

--- src/views/publications-view.tsx
+++ src/views/publications-view.tsx
@@ -9,7 +9,7 @@ export function PublicationsView() {
         "Silent Data Corruption: Advancing Detection, Diagnosis, and Mitigation Strategies",
       description:
         "Peter Domanski*, Mukarram Ali Faridi*, Gabriel Kaunang* (co-first author), Wilson Pradeep, Adit Singh, Muhammad Alfian Amrizal, Yanjing Li, Farshad Firouzi, and Krishnendu Chakrabarty",
-      conference: "The 43rd IEEE VLSI Test Symposium (VTS'25)",
+      conference: "The 43rd IEEE VLSI Test Symposium 2025 (VTS'25)",
       href: "https://ieeexplore.ieee.org/document/11022867",
     },
     {
@@ -18,7 +18,7 @@ export function PublicationsView() {
       description:
         "Fitra Rahmani Khasyah, Kadek Gemilang Santiyuda, Gabriel Kaunang, Faizal Makhrus, Muhammad Alfian Amrizal, and Hiroyuki Takizawa",
       conference:
-        "The 23rd International Conference on Parallel and Distributed Computing, Applications and Technologies (PDCAT'22)",
+        "The 23rd International Conference on Parallel and Distributed Computing, Applications and Technologies 2022 (PDCAT'22)",
       href: "https://link.springer.com/chapter/10.1007/978-3-031-29927-8_8",
     },
   ];
